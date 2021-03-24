b1f37f24197add06ba419653c8e405bdf05c8365
hijiki02 <rebanira02@gmail.com>
2021-03-24 12:15:03 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,18 @@
 <template>
-	<section class="section">
-		<div class="columns is-mobile">
-			<card title="Free" icon="github">
-				Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-			</card>
-
-			<card title="Responsive" icon="cellphone-link">
-				<b class="has-text-grey"> Every </b> component is responsive
-			</card>
-
-			<card title="Modern" icon="alert-decagram">
-				Built with <a href="https://vuejs.org/"> Vue.js </a> and
-				<a href="http://bulma.io/"> Bulma </a>
-			</card>
-
-			<card title="Lightweight" icon="arrange-bring-to-front">
-				No other internal dependency
-			</card>
+	<div class="hero has-background-black is-fullheight">
+		<div class="hero-body">
+			<clock />
 		</div>
-	</section>
+	</div>
 </template>
 
 <script>
-import Card from "~/components/Card"
+import Clock from "~/components/Clock.vue"
 
 export default {
-	name: "HomePage",
-
+	name: "Home",
 	components: {
-		Card,
+		Clock,
 	},
 }
 </script>
