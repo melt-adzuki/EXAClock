29bdcd2457e5db0fbd5ba17cab2ab9ffc4f627d3
hijiki02 <rebanira02@gmail.com>
2021-03-24 12:19:26 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ export default {
 		meta: [
 			{ charset: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ hid: "description", name: "description", content: "" },
+			{
+				hid: "description",
+				name: "description",
+				content: "An exact clock, which uses NTP to show time.",
+			},
 		],
 		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 	},
