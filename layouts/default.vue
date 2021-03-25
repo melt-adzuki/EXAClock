4889261b30afd9d97077bbd03d2abcc185f41734
hijiki02 <rebanira02@gmail.com>
2021-03-25 17:30:34 +0900

    ✨🖱️

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,34 @@
 <template>
-	<main>
+	<main :class="class_main">
 		<nuxt />
 	</main>
 </template>
 
+<script>
+export default {
+	data() {
+		return {
+			class_main: "has-cursor-hidden",
+		}
+	},
+	mounted() {
+		let timeout = false
+		window.addEventListener("mousemove", () => {
+			this.class_main = ""
+			if (timeout) clearTimeout(timeout)
+
+			timeout = setTimeout(() => (this.class_main = "has-cursor-hidden"), 1000)
+		})
+	},
+}
+</script>
+
 <style>
 body::-webkit-scrollbar {
 	display: none;
+}
+
+.has-cursor-hidden {
+	cursor: none;
 }
 </style>
